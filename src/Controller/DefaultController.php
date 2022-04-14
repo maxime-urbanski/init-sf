a17518134821ac67f23e6442832e066924b4fade
Urbanski Maxime <maxime@les-tilleuls.coop>
2022-04-14 14:22:15 +0200

    first controller & routes

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
 
 class DefaultController
 {
@@ -11,5 +12,17 @@ class DefaultController
     public function homepage(): Response
     {
         return new Response('coucou');
+    }
+
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null): Response
+    {
+        if($slug) {
+            $title = u(str_replace('-', ' ', $slug))->title(true);
+        } else {
+            $title = 'All Genres';
+        }
+
+        return new Response('Genre: '.$title);
     }
 }
